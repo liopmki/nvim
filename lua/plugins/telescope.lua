367efc8a8046f587062f479e7022d156a1ca4dae
liopmki <liuyiy4ng@outlook.com>
2023-07-18 20:17:16 +0800

    update

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,51 @@
+return {
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies ={
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
+
+            },
+        },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind live [G]rep under the folder' })
+            vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[space] Find buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+            vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+            vim.keymap.set('n', '<leader>/', function()
+                -- You can pass additional configuration to telescope to change theme, layout, etc.
+                require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 10,
+                    previewer = false,
+                })
+            end, { desc = '[/] Fuzzily search in current buffer' })
+
+            require('telescope').setup {
+                defaults = {
+                    mappings = {
+                        i = {
+                            ['<C-u>'] = false,
+                            ['<C-d>'] = false,
+                        },
+                    },
+                },
+                extensions = {
+                    fzf = {
+                        fuzzy = true,                    -- false will only do exact matching
+                        override_generic_sorter = true,  -- override the generic sorter
+                        override_file_sorter = true,     -- override the file sorter
+                        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                        -- the default case_mode is "smart_case"
+                    }
+                }
+            }
+            require('telescope').load_extension('fzf')
+        end
+
+    },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -28,7 +28,7 @@ return {
     },
     {
         "lukas-reineke/indent-blankline.nvim",
-        config = {
+        opts = {
             space_char_blankline = " ",
             show_current_context = true,
             show_current_context_start = true,
